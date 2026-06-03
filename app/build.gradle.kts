2d5c041ea56ea544b8c88223a5db2377f4026d48
Evangorder <ethanvangorder21@gmail.com>
2026-06-02 22:47:26 -0400

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+import org.gradle.kotlin.dsl.implementation
+
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.betweenus"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.betweenus"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.annotation)
+    implementation(libs.activity)
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.applandeo:material-calendar-view:1.9.2")
+
+
+    implementation(libs.circleimageview)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+}
